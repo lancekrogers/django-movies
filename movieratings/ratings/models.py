cde411558ebda3740e900d18d36510e709b3e497
Lance Rogers <lancekincaid1994@gmail.com>
2015-06-29 21:19:26 -0400

    set up models

--- movieratings/ratings/models.py
+++ movieratings/ratings/models.py
@@ -2,4 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-class
+class Rater(models.model):
+    #id = models.Field.db_column
+    rater_id = models.IntegerField()
+
+
+class Movies(models.model):
+    movie_id = models.IntegerField()
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+
+
+class Ratings(models.model):
+    rater_id = models.IntegerField()
+    movie_id = models.IntegerField()
+    rating = models.IntegerField()
+    time_stamp = models.DateField()
+
