170148f8252c4b36189d09490231877d329c8274
Lance Rogers <lancekincaid1994@gmail.com>
2015-06-30 19:35:53 -0400

    about to load fixtures

--- movieratings/movieratings/settings.py
+++ movieratings/movieratings/settings.py
@@ -102,3 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+FIXTURE_DIRS = (
+   'Django_Movies/django-movies/fixtures',
+)
---
+++ movieratings/ratings/migrations/0004_auto_20150630_1913.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def load_data(apps, schema_editor):
+
+    pass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ratings', '0003_auto_20150630_1736'),
+    ]
+
+    operations = [
+    ]
