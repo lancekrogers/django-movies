c40efbde26dcc67d580aad8fecd82e1d2825df89
Lance Rogers <lancekincaid1994@gmail.com>
2015-06-29 23:07:45 -0400

    Added admin

--- movieratings/ratings/admin.py
+++ movieratings/ratings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Movies, Rater, Ratings
 # Register your models here.
+
+admin.site.register(Movies)
+admin.site.register(Rater)
+admin.site.register(Ratings)
