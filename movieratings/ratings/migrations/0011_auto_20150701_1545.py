b39470da08a1c95d5d76e2ffac57ae37231af125
Lance Rogers <lancekincaid1994@gmail.com>
2015-07-01 16:37:21 -0400

    testing rating code

--- movieratings/ratings/migrations/0011_auto_20150701_1545.py
+++ movieratings/ratings/migrations/0011_auto_20150701_1545.py
@@ -2,7 +2,23 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from ratings.models import Rating, Rater, Movie
+import pandas as pd
 
+def load_rating_data(apps, schema_editor):
+    rating_df = pd.read_csv('ratings/fixtures/ratings.csv', names=["rater_id", "movie_id", "rating"])
+
+    for row in rating_df.iterrows():
+        rating_obj = row[1]
+        print(rating_obj)
+        raters = Rater.objects.get(rater=rating_obj.rater_id)
+        movies = Movie.objects.get(movie=rating_obj.movie_id)
+        Rating.objects.create(rater=raters, movie=movies, rating=rating_obj.rating)
+
+
+
+
+        raise Exception()
 
 class Migration(migrations.Migration):
 
@@ -11,4 +27,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(load_rating_data)
     ]
