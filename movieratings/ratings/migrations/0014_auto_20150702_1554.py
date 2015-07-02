f436ae7e1393b36cbfe3d0031f2577321a7f75bc
Lance Rogers <lancekincaid1994@gmail.com>
2015-07-02 18:44:43 -0400

    creating data migration for avg rating

---
+++ movieratings/ratings/migrations/0014_auto_20150702_1554.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+from ratings.models import Movie, Rating
+
+from statistics import mean
+
+def populate_avg(apps, schema_editor):
+    all_movies = Movie.objects.all()
+    for obj in all_movies:
+        rat_list = []
+        for r in Rating.objects.filter(movie = obj):
+            rat_list.append(r.rating)
+
+
+
+    Movie.objects.create(avg_rating=rating)
+    raise Exception()
+
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ratings', '0013_movie_avg_rating'),
+    ]
+
+    operations = [
+        migrations.RunPython(populate_avg)
+    ]
