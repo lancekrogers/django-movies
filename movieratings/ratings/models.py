6154ab3d43e739f71c67a7c98dd2a93406632a8f
Lance Rogers <lancekincaid1994@gmail.com>
2015-07-06 23:04:46 -0400

    made migration for user one to one model

--- movieratings/ratings/models.py
+++ movieratings/ratings/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Rater(models.Model):
+    user = models.OneToOneField(User, null=True)
     rater = models.IntegerField(primary_key=True)
     gender = models.CharField(max_length=1)
     age = models.IntegerField()
